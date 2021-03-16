d337ef7172ee3a2ee84bde92e0df0fdd89dd29cd
Adam Bodzak <bodzak.adam@gmail.com>
2021-03-16 16:33:50 +0100

    create simpli naplication

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ListWrapper from './components/ListWraper/ListWrapwer';
+import './index.css';
+
+const App = () => (
+    <div>
+        <ListWrapper />
+    </div>
+);
+
+export default App;
