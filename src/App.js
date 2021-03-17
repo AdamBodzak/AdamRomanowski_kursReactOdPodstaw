8aff735104370acf7f24f3754a75daea74df0ab4
Adam Bodzak <bodzak.adam@gmail.com>
2021-03-17 21:15:23 +0100

    add clas component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ListWrapper from './components/ListWraper/ListWrapwer';
+import MyComponent from './components/MyComponent/MyComponent';
 import './index.css';
 
 const App = () => (
     <div>
+        <MyComponent />
         <ListWrapper />
     </div>
 );
