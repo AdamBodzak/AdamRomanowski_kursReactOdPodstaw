6c43272525a603665f8213b860f98512c0dee1ef
Adam Bodzak <bodzak.adam@gmail.com>
2021-03-20 13:52:50 +0100

    refactoring code Form components with add new commponents name Input

---
+++ src/components/Input/Input.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Input.module.scss';
+
+const Input = ({tag: Tag, text, name, label, maxLength, required}) => {
+
+    return(
+        <div className={styles.formItem}>
+            <Tag
+                className={Tag === 'textarea' ? styles.textarea : styles.input}
+                type='text'
+                name={name}
+                id={name}
+                placeholder=" "
+                required={required}
+                maxLength={maxLength}
+            />
+            <label className={styles.label} htmlFor={name}>
+                {label}
+            </label>
+            <div className={styles.formItemBar} />
+        </div>
+    );
+};
+
+Input.propTypes = {
+    required: PropTypes.bool,
+    tag: PropTypes.string,
+    text: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    maxLength: PropTypes.number,
+};
+
+Input.defaultProps = {
+    required: true,
+    tag: 'input',
+    maxLength: 200,
+};
+
+export default Input;
