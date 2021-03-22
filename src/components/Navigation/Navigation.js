94edf14d23da4b8bf983a9468df3a59ba9c9015e
Adam Bodzak <bodzak.adam@gmail.com>
2021-03-22 15:21:37 +0100

    create Navigation components and add to Root.js file

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navigation = () => (
+    <ul>
+        <li><a href="/">Twitter</a></li>
+        <li><a href="/articles">Articles</a></li>
+        <li><a href="/notes">Notes</a></li>
+    </ul>
+);
+
+export default Navigation;
